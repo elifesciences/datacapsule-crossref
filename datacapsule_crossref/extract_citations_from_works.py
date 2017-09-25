e6de23e4ca2bf93099e4b02db5e1a571c5f4b690
Daniel Ecer <de-code@users.noreply.github.com>
2017-09-25 22:03:05 +0100

    also run parsing in separate process; optionally as a thread or process

--- datacapsule_crossref/extract_citations_from_works.py
+++ datacapsule_crossref/extract_citations_from_works.py
@@ -5,8 +5,9 @@ import logging
 import os
 import json
 from zipfile import ZipFile
-import codecs
 import multiprocessing as mp
+from threading import Thread
+from queue import Queue
 
 from tqdm import tqdm
 
@@ -34,6 +35,15 @@ def get_args_parser():
     '--delimiter', type=str, required=False,
     help='output file delimiter (otherwise determined by filename)'
   )
+  parser.add_argument(
+    '--num-workers', type=int, required=False,
+    help='number of workers to use'
+  )
+  parser.add_argument(
+    '--multi-processing', required=False,
+    action='store_true',
+    help='specify this flag, to enable multi processing'
+  )
   return parser
 
 def clean_doi(doi):
@@ -52,25 +62,66 @@ def extract_citations_from_response(response):
   for work in items:
     yield extract_citations_from_work(work)
 
-def read_zip_to_queue(input_file, queue):
+def read_zip_to_queue(input_file, output_queue, num_output_processes):
   with ZipFile(input_file, 'r') as zip_f:
     names = zip_f.namelist()
     get_logger().info('files: %s', len(names))
-    for name in tqdm(names):
-      queue.put(zip_f.read(name))
-  queue.put(None)
+    for name in tqdm(names, smoothing=0.1):
+      output_queue.put(zip_f.read(name))
+  for _ in range(num_output_processes):
+    output_queue.put(None)
 
-def iter_zip_citations(input_file):
-  response_queue = mp.Queue(10)
-  zip_process = mp.Process(target=read_zip_to_queue, args=(input_file, response_queue))
-  zip_process.start()
+def extract_citations_to_queue(input_queue, output_queue):
+  for item in iter(input_queue.get, None):
+    response = json.loads(item.decode('utf-8'))
+    output_queue.put([
+      (doi, citation_dois)
+      for doi, citation_dois in extract_citations_from_response(response)
+    ])
+  output_queue.put(None)
 
-  while True:
-    response = response_queue.get()
-    if response is None:
-      break
-    response = json.loads(response.decode('utf-8'))
-    for doi, citation_dois in extract_citations_from_response(response):
+def make_daemon(p):
+  p.daemon = True
+  return p
+
+def iter_zip_citations(input_file, num_workers=None, multi_processing=None):
+  cpu_count = mp.cpu_count()
+  if num_workers is None:
+    # substract 2: one for the producer and one for the main (output)
+    num_workers = max(1, cpu_count - 2)
+
+  if not multi_processing:
+    Q = Queue
+    P = Thread
+  else:
+    Q = mp.Queue
+    P = mp.Process
+
+  get_logger().info(
+    'num_workers: %d (cpu count: %d, mp: %s)', num_workers, cpu_count, multi_processing
+  )
+
+  response_queue = Q(10 * num_workers)
+  citations_queue = Q(10 * num_workers)
+
+  for _ in range(num_workers):
+    make_daemon(P(
+      target=extract_citations_to_queue,
+      args=(response_queue, citations_queue)
+    )).start()
+
+  make_daemon(P(
+    target=read_zip_to_queue,
+    args=(input_file, response_queue, num_workers)
+  )).start()
+
+  workers_running = num_workers
+  while workers_running > 0:
+    item = citations_queue.get()
+    if item is None:
+      workers_running -= 1
+      continue
+    for doi, citation_dois in item:
       if doi:
         yield doi, citation_dois
 
@@ -85,11 +136,17 @@ def extract_citations_from_works_direct(argv):
   output_file = args.output_file
   makedirs(os.path.basename(output_file), exist_ok=True)
 
+  get_logger().info('output_file: %s', output_file)
+
   write_csv(
     output_file,
     ['citing_doi', 'cited_doi'],
     flatten_citations(
-      iter_zip_citations(args.input_file)
+      iter_zip_citations(
+        args.input_file,
+        num_workers=args.num_workers,
+        multi_processing=args.multi_processing
+      )
     ),
     delimiter=args.delimiter
   )
