0c50175497d1398340995c39f9a566ca849d146e
Daniel Ecer <de-code@users.noreply.github.com>
2017-09-25 15:54:34 +0100

    clean dois

--- datacapsule_crossref/extract_citations_from_works.py
+++ datacapsule_crossref/extract_citations_from_works.py
@@ -31,11 +31,14 @@ def get_args_parser():
   )
   return parser
 
+def clean_doi(doi):
+  return doi.strip().replace('\n', ' ') if doi else None
+
 def extract_citations_from_work(work):
-  doi = work.get('DOI')
+  doi = clean_doi(work.get('DOI'))
   references = work.get('reference', [])
   citation_dois = [r.get('DOI') for r in references]
-  citation_dois = [doi for doi in citation_dois if doi]
+  citation_dois = [clean_doi(doi) for doi in citation_dois if doi]
   return doi, citation_dois
 
 def extract_citations_from_response(response):
