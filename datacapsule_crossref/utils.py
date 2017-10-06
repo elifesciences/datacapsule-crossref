f10b103f027f24fe289e6359f8d81166b42a4200
Daniel Ecer <de-code@users.noreply.github.com>
2017-10-06 13:03:03 +0100

    detect compression based on filename; process file contents with tqdm progress

--- datacapsule_crossref/utils.py
+++ datacapsule_crossref/utils.py
@@ -5,8 +5,10 @@ import csv
 import six
 import requests
 from requests.packages.urllib3 import Retry
+from tqdm import tqdm
 
 TEMP_FILE_SUFFIX = '.part'
+GZIP_COMPRESSION = 'gzip'
 
 def makedirs(path, exist_ok=False):
   try:
@@ -44,11 +46,51 @@ def gzip_open(filename, mode):
   else:
     return gzip.open(filename, mode)
 
-def optionally_compressed_open(filename, mode):
+def compression_by_filename(filename):
   if filename.endswith('.gz') or filename.endswith('.gz' + TEMP_FILE_SUFFIX):
+    return GZIP_COMPRESSION
+  else:
+    return None
+
+def optionally_compressed_open(filename, mode):
+  if compression_by_filename(filename) == GZIP_COMPRESSION:
     return gzip_open(filename, mode)
   else:
     return open(filename, mode)
+
+def csv_delimiter_by_filename(filename):
+  if '.tsv' in filename:
+    return '\t'
+  else:
+    return ','
+
+def iter_read_csv_columns(filename, columns, delimiter=None):
+  if delimiter is None:
+    delimiter = csv_delimiter_by_filename(filename)
+  with optionally_compressed_open(filename, 'r') as csv_f:
+    csv_reader = csv.reader(csv_f, delimiter=delimiter)
+    first_row = True
+    indices = None
+    for row in csv_reader:
+      if first_row:
+        indices = [row.index(c) for c in columns]
+        first_row = False
+      else:
+        yield [row[i] for i in indices]
+
+def iter_with_file_read_progress(filename, generator):
+  with open(filename, 'rb') as fp:
+    fp.seek(0, os.SEEK_END)
+    total = fp.tell()
+    fp.seek(0)
+    previous_offset = 0
+    with tqdm(unit='B', leave=False, total=total, unit_scale=1024 * 1024) as pbar:
+      for item in generator(fp):
+        current_offset = fp.tell()
+        if current_offset != previous_offset:
+          pbar.update(current_offset - previous_offset)
+        previous_offset = current_offset
+        yield item
 
 def open_csv_output(filename):
   return optionally_compressed_open(filename, 'w')
@@ -66,12 +108,6 @@ def write_csv_rows(writer, iterable):
 
 def write_csv_row(writer, row):
   write_csv_rows(writer, [row])
-
-def csv_delimiter_by_filename(filename):
-  if '.tsv' in filename:
-    return '\t'
-  else:
-    return ','
 
 def write_csv(filename, columns, iterable, delimiter=None):
   if delimiter is None:
