a630c8bf3a79c6e4659a7a6adc8ecb8e71194198
Daniel Ecer <de-code@users.noreply.github.com>
2017-10-06 13:03:27 +0100

    normalise doi key

--- datacapsule_crossref/doi_utils.py
+++ datacapsule_crossref/doi_utils.py
@@ -1,2 +1,15 @@
+
+# clean DOI without changing it too much
 def clean_doi(doi):
-  return doi.strip().replace('\n', '').replace('\t', '').replace('"', '') if doi else None
+  return doi.strip().replace('\n', '').replace('\t', '') if doi else None
+
+def doi_to_normalised_key(doi):
+  return (
+    doi
+    .strip()
+    .lower()
+    .replace('\n', '')
+    .replace('\t', '')
+    .replace('"', '')
+    .replace('=', '/')
+  ) if doi else None
--- datacapsule_crossref/doi_utils_test.py
+++ datacapsule_crossref/doi_utils_test.py
@@ -1,4 +1,7 @@
-from datacapsule_crossref.doi_utils import clean_doi
+from datacapsule_crossref.doi_utils import (
+  clean_doi,
+  doi_to_normalised_key
+)
 
 SOME_DOI = '10.12345/path'
 
@@ -18,5 +21,27 @@ class TestCleanDoi(object):
   def test_should_remove_tabs_within_doi(self):
     assert clean_doi('{}\t{}'.format(SOME_DOI[:2], SOME_DOI[2:])) == SOME_DOI
 
+class TestNormaliseDoi(object):
+  def test_should_return_none_if_doi_is_none(self):
+    assert doi_to_normalised_key(None) is None
+
+  def test_should_convert_to_lower_case(self):
+    assert doi_to_normalised_key(SOME_DOI.upper()) == SOME_DOI.lower()
+
+  def test_should_strip_surround_spaces(self):
+    assert doi_to_normalised_key(' {} '.format(SOME_DOI)) == SOME_DOI
+
+  def test_should_strip_surround_line_feeds(self):
+    assert doi_to_normalised_key('\n{}\n'.format(SOME_DOI)) == SOME_DOI
+
+  def test_should_remove_line_feeds_within_doi(self):
+    assert doi_to_normalised_key('{}\n{}'.format(SOME_DOI[:2], SOME_DOI[2:])) == SOME_DOI
+
+  def test_should_remove_tabs_within_doi(self):
+    assert doi_to_normalised_key('{}\t{}'.format(SOME_DOI[:2], SOME_DOI[2:])) == SOME_DOI
+
   def test_should_remove_double_quotes_within_doi(self):
-    assert clean_doi('{}"{}'.format(SOME_DOI[:2], SOME_DOI[2:])) == SOME_DOI
+    assert doi_to_normalised_key('{}"{}'.format(SOME_DOI[:2], SOME_DOI[2:])) == SOME_DOI
+
+  def test_should_replace_equals_with_forward_slash(self):
+    assert doi_to_normalised_key(SOME_DOI.replace('/', '=')) == SOME_DOI
